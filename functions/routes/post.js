4d17a8c3515a33a2c2f1e4296369e427a26ad871
dLswStudy <devLswStudy@gmail.com>
2024-03-08 12:24:42 +0900

    위키페이지 route - 등록(수정), 상세수정/상세삭제/상세여러개삭제 (추가)

--- functions/routes/post.js
+++ functions/routes/post.js
@@ -1,6 +1,6 @@
 const moment = require('moment');
 const logger = require("firebase-functions/logger");
-const {addPost, getPostById} = require("../service/PostService");
+const {addPost, getPostById, putPostById, deletePostById, convertToRaw, deletePostByIds} = require("../service/PostService");
 const express = require("express");
 const router = express.Router();
 
@@ -19,15 +19,14 @@ function postRouter(firestore) {
         try {
             logger.info("postPostOne");
 
-            const now = moment().add(9, 'hour').format('YYYYMMDDHHmmss');
-            logger.info("postPostOne now", now);
-
+            let content = req.body.content;
+            //Test 생성용 (Front 의 react-draft-wysiwyg 에디터용)
+            if(typeof content === 'string') content = convertToRaw(content)
             const postData = {
                 title: req.body.title,
-                content: req.body.content,
-                instDT: now,
-                modfDT: now
+                content: content //
             };
+            console.log("postData = ", postData);
 
             const addedData = await addPost(firestore, postData)
             res.status(200).send(addedData);
@@ -45,6 +44,40 @@ function postRouter(firestore) {
             res.status(500).send(error.message);
         }
     });
+    //위키페이지 상세 업데이트
+    router.put("/:postId", async (req, res) => {
+        try {
+            const postId = req.params.postId;
+            const data = req.body;
+            await putPostById(firestore, postId, data);
+            res.status(200).send('success');
+        } catch (error) {
+            res.status(500).send(error.message);
+        }
+    });
+    // 상세 페이지 삭제
+    router.delete("/:postId", async (req, res) => {
+        try {
+            const postId = req.params.postId;
+            await deletePostById(firestore, postId); // PostService에서 제공하는 삭제 함수 호출
+            res.status(200).send({ message: 'Post deleted successfully' });
+        } catch (error) {
+            logger.error(`Error deleting post: ${error}`);
+            res.status(500).send({ message: error.message });
+        }
+    });
+    // 상세 페이지 여러개 삭제
+    router.delete("/:postId1/:postId2", async (req, res) => {
+        try {
+            const { postId1, postId2 } = req.params;
+            await deletePostByIds(firestore, postId1, postId2); // PostService에서 제공하는 삭제 함수 호출
+            res.status(200).send({ message: 'Post deleted successfully' });
+        } catch (error) {
+            logger.error(`Error deleting post: ${error}`);
+            res.status(500).send({ message: error.message });
+        }
+    });
+
 
     return router;
 }
--- functions/service/PostService.js
+++ functions/service/PostService.js
@@ -1,5 +1,11 @@
 const moment = require("moment");
+const logger = require("firebase-functions/logger");
 const collectionPath = 'Posts'
+
+function calcNow() {
+    const now = moment().add(9, 'hour').format('YYYYMMDDHHmmss');
+    return now;
+}
 
 async function addPost(db, data) {
     const indexDocRef = db.collection('Index').doc('Post');
@@ -14,6 +20,10 @@ async function addPost(db, data) {
 
         // Posts 컬렉션에 새 문서 추가
         const newPostRef = postsCollRef.doc(nextIndex.toString());
+        const now = calcNow();
+        logger.info("postPostOne now", now);
+        data.instDT = now
+        data.modfDT = now
         transaction.set(newPostRef, data);
 
         // Index 문서의 index 필드 업데이트
@@ -34,16 +44,17 @@ async function getPostById(db, postId) {
         data.next = {title: ''}
 
         const idx = await getPrevNextIdx(db, postId)
-        console.log('idx',idx)
         if (idx.prevIdx>=0) {
             const prevDoc = db.collection(collectionPath).doc(idx.prevIdx + '');
             const prevSnap = await prevDoc.get();
             data.prev.title = prevSnap.data().title
+            data.prev.postId = idx.prevIdx
         }
         if (idx.nextIdx>=0) {
             const nextDoc = db.collection(collectionPath).doc(idx.nextIdx + '');
             const nextSnap = await nextDoc.get();
             data.next.title = nextSnap.data().title
+            data.next.postId = idx.nextIdx
         }
 
         return data;
@@ -78,8 +89,86 @@ async function getPrevNextIdx(db, postId) {
     }
 }
 
+async function putPostById(db, postId, data) {
+    try {
+        const postRef = db.collection(collectionPath).doc(postId);
+        data.modfDT = calcNow()
+        await postRef.update(data);
+    } catch (error) {
+        console.error('Error[putPostById]:', error);
+        throw new Error('문서 업데이트 중 오류가 발생했습니다.');
+    }
+}
+
+async function deletePostById(db, postId) {
+    const postRef = db.collection(collectionPath).doc(postId);
+    const snapshot = await postRef.get();
+    if(!snapshot.exists) throw new Error('삭제할 문서가 존재하지 않습니다.')
+    await postRef.delete();
+}
+async function deletePostByIds(db, postId1, postId2) {
+    for (let id = Number(postId1); id <= Number(postId2); id++) {
+        const postRef = db.collection(collectionPath).doc(id.toString());
+        const snapshot = await postRef.get();
+        if(!snapshot.exists) throw new Error('삭제할 문서가 존재하지 않습니다. postId: '+id)
+        await postRef.delete();
+    }
+}
+
+
+
+function convertTitlesToLinks(content, docArr) {
+    docArr.forEach(doc => {
+        // 이미 태그로 감싸진 title을 제외하기 위한 정규 표현식
+        const regex = new RegExp(`(?<!<[^>]*?)${doc.title}(?![^<]*?>)`, "g");
+        content = content.replace(regex, (match) => {
+            // 이미 다른 태그로 감싸진 경우를 확인하기 위한 추가 검사
+            if (/<[^>]+>/.test(match)) {
+                return match; // 이미 다른 태그로 감싸진 경우 변경하지 않음
+            }
+            return `<a href="#" onClick="()=>moveTo(${doc.index})">${doc.title}</a>`;
+        });
+    });
+    return content;
+}
+
+async function contentWithTag(db, content) {
+    try {
+        const snapshot = await db.collection(collectionPath).orderBy('index', 'asc').get();
+        const posts = snapshot.docs.map(doc => ({...doc.data()}));
+        data.modfDT = calcNow()
+        await postRef.update(data);
+    } catch (error) {
+        console.error('Error[putPostById]:', error);
+        throw new Error('문서 업데이트 중 오류가 발생했습니다.');
+    }
+}
+
+function convertToRaw(content){
+    const unit = {
+        "entityMap": {},
+        "blocks": [
+            {
+                "depth": 0,
+                "data": {},
+                "inlineStyleRanges": [],
+                "text": "",
+                "type": "unstyled",
+                "key": "6p1cd",
+                "entityRanges": []
+            }
+        ]
+    }
+    unit.blocks[0].text = content
+    return unit
+}
+
 
 module.exports = {
     addPost,
     getPostById,
+    putPostById,
+    deletePostById,
+    deletePostByIds,
+    convertToRaw
 }
